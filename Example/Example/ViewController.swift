4e9f5027a71ac0752b6c9edd143415629a44e1ec
Yanni Wang <yanni.wang@Yannis-MacBook-Pro.local>
2021-03-17 11:36:10 +0800

    example

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -11,7 +11,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // outer
+        let outerContent = UIView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height * 3))
+        let outerGradientLayer = CAGradientLayer()
+        outerGradientLayer.colors = [UIColor.orange.cgColor, UIColor.blue.cgColor]
+        outerGradientLayer.locations = [0, 1]
+        outerGradientLayer.frame = outerContent.bounds
+        outerContent.layer.addSublayer(outerGradientLayer)
+        
+        let outerScrollView = UIScrollView.init(frame: self.view.bounds)
+        outerScrollView.addSubview(outerContent)
+        outerScrollView.contentSize = outerContent.frame.size
+        self.view.addSubview(outerScrollView)
+        
+        // inside
+        let insideContent = UIView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height * 2))
+        let insideGradientLayer = CAGradientLayer()
+        insideGradientLayer.colors = [UIColor.yellow.cgColor, UIColor.red.cgColor]
+        insideGradientLayer.locations = [0, 1]
+        insideGradientLayer.frame = insideContent.bounds
+        insideContent.layer.addSublayer(insideGradientLayer)
+        
+        let insideScrollView = UIScrollView.init(frame: CGRect.init(x: 0, y: self.view.bounds.height, width: self.view.bounds.width, height: self.view.bounds.height))
+        insideScrollView.addSubview(insideContent)
+        insideScrollView.contentSize = insideContent.frame.size
+        outerContent.addSubview(insideScrollView)
     }
 
 
